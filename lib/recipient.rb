db879e6754a0245f426be39e0b2b8222fd4201fa
Kareha Agesa <kmaagesa@gmail.com>
2020-10-06 14:33:43 -0700

    repetitive

---
+++ lib/recipient.rb
@@ -0,0 +1,24 @@
+require 'httparty'
+
+class Recipient
+  attr_reader :slack_id, :name
+
+  def initialize(slack_id:, name:)
+    @slack_id = slack_id
+    @name = name
+  end
+
+  def self.get(url, params)
+    response = HTTParty.get(url, query: params)
+    return response
+  end
+
+  def self.list_all
+    raise NotImplementedError.new("Implement me in a child class")
+  end
+
+  def details
+    raise NotImplementedError.new("Implement me in a child class")
+  end
+
+end
