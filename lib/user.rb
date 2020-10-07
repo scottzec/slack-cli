867e2f149a0c570cf1e7f235d0d13c815f916ac1
Leah Scott-Zechlin <scottzec@gmail.com>
2020-10-06 20:57:48 -0700

    re-added driver file

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,13 @@
 require_relative 'recipient'
+require 'dotenv'
+
+Dotenv.load
+
 
 class User < Recipient
   attr_reader :username, :real_name
+
+  USER_LIST_URL = "https://slack.com/api/users.list"
 
   def initialize(slack_id:, username:, real_name:, name:)
     super(slack_id, name)
@@ -9,4 +15,9 @@ class User < Recipient
     @username = username
     @real_name = real_name
   end
+
+  def self.list_all
+    self.get(USER_LIST_URL, query)
+  end
+
 end
