2ea84920af998026265bc07a3043a2ca72b9140e
Leah Scott-Zechlin <scottzec@gmail.com>
2020-10-08 16:00:17 -0700

    send message to channel done

--- lib/recipient.rb
+++ lib/recipient.rb
@@ -1,4 +1,7 @@
 require 'httparty'
+require 'dotenv'
+
+Dotenv.load
 
 class Recipient
   attr_reader :slack_id
@@ -13,12 +16,37 @@ class Recipient
     return response
   end
 
+  def send_message(message)
+
+    body_param = {
+        token: ENV["SLACK_TOKEN"],
+        text: message,
+        channel: @slack_id
+    }
+    response = HTTParty.post(
+        "https://slack.com/api/chat.postMessage",
+        body: body_param,
+        headers: { 'Content-Type' => 'application/x-www-form-urlencoded' }
+    )
+
+    response_string = validate_post(response)
+
+    return response_string
+  end
+
   def self.list_all
     raise NotImplementedError.new("Implement me in a child class")
   end
 
   def details
     raise NotImplementedError.new("Implement me in a child class")
+  end
+
+  private
+
+  def validate_post(response)
+    raise ArgumentError.new("API call failed with code #{response.code} and error #{response['error']}") if response["ok"] == false
+    return "Thank you this message was sent"
   end
 
   # Consider testing get method here instead of in user and recipient test files
--- lib/workspace.rb
+++ lib/workspace.rb
@@ -1,13 +1,18 @@
 require_relative 'user'
 require_relative 'channel'
 
-class Workspace
+class Workspace < Recipient
 
   attr_reader :users, :channels
 
   def initialize
     @users = User.list_all
     @channels = Channel.list_all
+    @selected = nil
+  end
+
+  def send_message(message)
+    @selected.send_message(message)
   end
 
   def select_user(user, property)
