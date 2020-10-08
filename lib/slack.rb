25e588efe2bdf1932072f58c866ebdc471661ec3
Kareha Agesa <kmaagesa@gmail.com>
2020-10-07 21:05:27 -0700

    select and show channel details in workspace

--- lib/slack.rb
+++ lib/slack.rb
@@ -4,10 +4,13 @@ require_relative "workspace"
 require "table_print"
 
 
-def program_prompt(users, channels)
+def program_prompt(users, channels, workspace)
   puts "Please enter your selected option:"
   puts "- list users"
   puts "- list channels"
+  puts "- select channel"
+  puts "- select user"
+  puts "- details"
   puts "- quit"
 
   input = validate_input(gets.chomp)
@@ -16,6 +19,17 @@ def program_prompt(users, channels)
     tp users
   when "list channels"
     tp channels
+  when "select channel"
+    puts "Please enter the channel you want to see information for. \nDo you want to enter an ID or a name?"
+    property = gets.chomp
+    puts "Thanks! Please enter it"
+    user_channel = gets.chomp
+    channel = workspace.select_channel(user_channel, property)
+    if channel == nil
+      puts "This channel doesn't exist! Exiting..."
+    end
+  when "details"
+    puts workspace.show_details
   when "quit"
     exit
   end
@@ -38,9 +52,9 @@ def main
 
   puts "There are #{users.length} users and #{channels.length} channels in this workspace"
 
-  input = program_prompt(users, channels)
+  input = program_prompt(users, channels, workspace)
   until input == "quit"
-    input = program_prompt(users, channels)
+    input = program_prompt(users, channels, workspace)
   end
   puts "Thank you for using the Ada Slack CLI"
 end
